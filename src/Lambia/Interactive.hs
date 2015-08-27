5b68f6f7723c1aa0cefb5d217c8c0dce7d6f0fe6
phi16 <phi16.256@gmail.com>
2015-08-27 11:28:49 +0900

    Allow empty line

--- src/Lambia/Interactive.hs
+++ src/Lambia/Interactive.hs
@@ -2,7 +2,8 @@
 
 module Lambia.Interactive (interactive) where
 
-import Prelude hiding (getLine, putStr, putStrLn, null)
+import Prelude hiding (getLine, putStr, putStrLn, null, filter)
+import Data.Char (isSpace)
 import Data.ByteString.Char8 hiding (append)
 import qualified Data.ByteString.Char8 as B
 import Control.Applicative hiding (empty)
@@ -25,30 +26,33 @@ ev :: ByteString -> Act ()
 ev str = do
   lift $ putStr $ if null str then "> " else "| "
   x <- flip snoc '\n' <$> lift getLine
-  let ss = str`B.append`x
-  case parseLines ss of
-    Left err -> case "unexpected \'\\NUL\'"`isInfixOf`err of
-      True -> do
-        ev ss
-      False -> do
-        lift $ putStrLn err
-        ev ""
-    Right e -> do
-      s <- get
-      let
-        (r,s') = flip runState s $ runExceptT $ case e of
-          Left d -> Left <$> ixDecl d
-          Right x -> Right <$> ixExpr x
-      s'' <- case r of
-        Left f -> do
-          lift $ putStrLn f
-          return s'
-        Right (Left d') -> return s'
-        Right (Right e') -> do
-          let v = fst $ apply e'
-          lift $ putStrLn $ pack $ show v
-          return $ case s' of
-            Status a u -> Status a $ append "it" Nothing v u
-      put s''
-      ev ""
+  if null $ filter (not . isSpace) x
+    then ev str
+    else do
+      let ss = str`B.append`x
+      case parseLines ss of
+        Left err -> case "unexpected \'\\NUL\'"`isInfixOf`err of
+          True -> do
+            ev ss
+          False -> do
+            lift $ putStrLn err
+            ev ""
+        Right e -> do
+          s <- get
+          let
+            (r,s') = flip runState s $ runExceptT $ case e of
+              Left d -> Left <$> ixDecl d
+              Right x -> Right <$> ixExpr x
+          s'' <- case r of
+            Left f -> do
+              lift $ putStrLn f
+              return s'
+            Right (Left d') -> return s'
+            Right (Right e') -> do
+              let v = fst $ apply e'
+              lift $ putStrLn $ pack $ show v
+              return $ case s' of
+                Status a u -> Status a $ append "it" Nothing v u
+          put s''
+          ev ""
 
